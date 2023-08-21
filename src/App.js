e3a5869352d97e6ba384d31b4904fa65922f748b
LeandroCarolino01 <carolino.leandro@gmail.com>
2023-08-21 18:07:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import MovieCard from "./components/MovieCard";
 
 function App() {
+  const API_URL = "https://api.themoviedb.org/3/";
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover";
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}/${type}/movie`, {
+      params: {
+        api_key: process.env.REACT_APP_MOVIE_API_KEY,
+        query: searchKey,
+      },
+    });
+    setMovies(results);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const renderMovies = () =>
+    movies.map((movie) => <MovieCard key={movie.id} movie={movie} />);
+
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(searchKey);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Movie Trailer</h1>
+        <form onSubmit={searchMovies}>
+          <input type="text" onChange={(e) => setSearchKey(e.target.value)} />
+          <button type="submit">Search!</button>
+        </form>
       </header>
+      {searchKey}
+      <div className="container">{renderMovies()}</div>
     </div>
   );
 }
